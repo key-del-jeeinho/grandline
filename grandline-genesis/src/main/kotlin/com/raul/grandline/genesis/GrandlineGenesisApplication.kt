84aacc1ac03dffb2e8b21b11395ead7f1fdce75c
지인호 [Raul] <raul@daangn.com>
2023-05-27 18:35:55 +0900

    add: initial commit

---
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/GrandlineGenesisApplication.kt
@@ -0,0 +1,11 @@
+package com.raul.grandline.genesis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrandlineGenesisApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrandlineGenesisApplication>(*args)
+}
---
+++ grandline-genesis/src/test/kotlin/com/raul/grandline/genesis/GrandlineGenesisApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.raul.grandline.genesis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrandlineGenesisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
