bcf575034902792d3e8379be5981e2d1bc6af6ae
지인호 [Raul] <raul@daangn.com>
2023-05-28 14:31:02 +0900

    add: AccountPersistenceAdapter

---
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/account/adapter/data/AccountPersistenceAdapter.kt
@@ -0,0 +1,20 @@
+package com.raul.grandline.genesis.account.adapter.data
+
+import com.raul.grandline.genesis.account.adapter.AccountConverter.Companion.convert
+import com.raul.grandline.genesis.account.adapter.data.AccountEntityConverter.Companion.convert
+import com.raul.grandline.genesis.account.adapter.data.entity.AccountEntity
+import com.raul.grandline.genesis.account.adapter.data.repository.AccountRepository
+import com.raul.grandline.genesis.account.application.port.output.SaveAccountOutput
+import com.raul.grandline.genesis.account.domain.Account
+import org.springframework.stereotype.Component
+
+@Component
+class AccountPersistenceAdapter(
+    private val accountRepository: AccountRepository,
+) : SaveAccountOutput {
+    override fun saveAccount(account: Account): Account {
+        val entity = convert(account) to AccountEntity
+        val savedEntity = accountRepository.save(entity)
+        return convert(savedEntity) to Account
+    }
+}
