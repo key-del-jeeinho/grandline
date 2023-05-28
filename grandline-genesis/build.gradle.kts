67229fa98b0d90a1b690a9d9b944916d25148f73
지인호 [Raul] <raul@daangn.com>
2023-05-28 23:57:57 +0900

    add: micrometer for prometheus

--- grandline-genesis/build.gradle.kts
+++ grandline-genesis/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-core")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
