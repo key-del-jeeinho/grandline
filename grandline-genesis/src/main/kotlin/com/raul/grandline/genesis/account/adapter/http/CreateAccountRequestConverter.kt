32c112572d04e70de52fd914d23fc4fc6cfde31a
지인호 [Raul] <raul@daangn.com>
2023-05-28 15:23:24 +0900

    add: CreateAccountRequestConverter for convert from request

---
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/account/adapter/http/CreateAccountRequestConverter.kt
@@ -0,0 +1,23 @@
+package com.raul.grandline.genesis.account.adapter.http
+
+import com.raul.grandline.genesis.account.adapter.http.request.CreateAccountRequest
+import com.raul.grandline.genesis.account.domain.common.AccountIdentifier
+import com.raul.grandline.genesis.account.domain.common.AccountRawPassword
+import com.raul.grandline.genesis.account.domain.create.CreateAccount
+
+class CreateAccountRequestConverter(
+    private val createAccountRequest: CreateAccountRequest,
+) {
+    infix fun to(toWhat: CreateAccount.Companion): CreateAccount = createAccountRequest.run {
+        CreateAccount(
+            identifier = AccountIdentifier(identifier),
+            rawPassword = AccountRawPassword(rawPassword),
+        )
+    }
+
+    companion object {
+        infix fun convert(
+            createAccountRequest: CreateAccountRequest,
+        ): CreateAccountRequestConverter = CreateAccountRequestConverter(createAccountRequest)
+    }
+}
