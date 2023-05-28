31acb98312e82ae700b2d9d797feaa5d85177bc2
지인호 [Raul] <raul@daangn.com>
2023-05-28 13:45:22 +0900

    fix: AccountService.createAccount() (event publish logic and return definition)

--- grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/account/application/service/AccountService.kt
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/account/application/service/AccountService.kt
@@ -3,15 +3,28 @@ package com.raul.grandline.genesis.account.application.service
 import com.raul.grandline.genesis.account.application.port.input.CreateAccountUseCase
 import com.raul.grandline.genesis.account.application.port.input.EncodePasswordOutput
 import com.raul.grandline.genesis.account.application.port.input.EncodePasswordOutput.Companion.invoke
+import com.raul.grandline.genesis.account.application.service.SaveAccountOutput.Companion.invoke
 import com.raul.grandline.genesis.account.domain.Account
 import com.raul.grandline.genesis.account.domain.AccountBuilder.Companion.create
+import com.raul.grandline.genesis.account.domain.AccountCreatedEvent
+import com.raul.grandline.genesis.account.domain.AccountCreatedEventBuilder.Companion.create
 import com.raul.grandline.genesis.account.domain.CreateAccount
+import com.raul.grandline.genesis.global.invoke
+import org.springframework.context.ApplicationEventPublisher
 
 class AccountService(
-    private val encodePassword: EncodePasswordOutput
+    private val encodePassword: EncodePasswordOutput,
+    private val saveAccount: SaveAccountOutput,
+    private val publishEvent: ApplicationEventPublisher
 ) : CreateAccountUseCase {
     override fun createAccount(domain: CreateAccount): Account {
         val encodedPassword = encodePassword(domain.rawPassword)
         val account = create(Account) by (domain to encodedPassword)
+        val savedAccount = saveAccount(account)
+
+        val event = create(AccountCreatedEvent) by savedAccount
+        publishEvent(event)
+
+        return savedAccount
     }
 }
