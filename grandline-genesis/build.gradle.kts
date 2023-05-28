639f6ea17a91c6bbff2c3cba7902b285436b3a3c
지인호 [Raul] <raul@daangn.com>
2023-05-28 16:14:28 +0900

    add: ktlint plugin to gradle

--- grandline-genesis/build.gradle.kts
+++ grandline-genesis/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21"
