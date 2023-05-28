a58f1b0ed9dfb86525d6ce5baa8d103f1c0ab8e2
지인호 [Raul] <raul@daangn.com>
2023-05-28 11:17:13 +0900

    add: CreateAccountUseCase

--- grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/account/application/port/input/CreateAccountUseCase.kt
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/account/application/port/input/CreateAccountUseCase.kt
@@ -1,4 +1,11 @@
 package com.raul.grandline.genesis.account.application.port.input
 
-interface CreateAccountUsecase {
+import com.raul.grandline.genesis.account.domain.Account
+import com.raul.grandline.genesis.account.domain.CreateAccount
+
+interface CreateAccountUseCase {
+    fun createAccount(domain: CreateAccount): Account
+    companion object {
+        operator fun CreateAccountUseCase.invoke(domain: CreateAccount): Account = createAccount(domain)
+    }
 }
