fce874c46d9b024eceb4bc629b0b6d50b6359ece
지인호 [Raul] <raul@daangn.com>
2023-05-28 15:08:36 +0900

    add: PasswordEncoderConfig

---
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/global/PasswordEncoderConfig.kt
@@ -0,0 +1,12 @@
+package com.raul.grandline.genesis.global
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class PasswordEncoderConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+}
