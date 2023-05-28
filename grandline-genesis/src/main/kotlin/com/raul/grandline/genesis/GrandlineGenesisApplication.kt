a825f0eb72783c5b3d7d199583f412453b2b058c
지인호 [Raul] <raul@daangn.com>
2023-05-28 17:12:04 +0900

    add: EnableWebSecurity annotation to main class

--- grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/GrandlineGenesisApplication.kt
+++ grandline-genesis/src/main/kotlin/com/raul/grandline/genesis/GrandlineGenesisApplication.kt
@@ -2,8 +2,10 @@ package com.raul.grandline.genesis
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
+@EnableWebSecurity
 class GrandlineGenesisApplication
 
 fun main(args: Array<String>) {
