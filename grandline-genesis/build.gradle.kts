29ee741a8bebcdab818856f7f13ccad3e78a99d7
지인호 [Raul] <raul@daangn.com>
2023-05-28 17:10:32 +0900

    add: spring actuator

--- grandline-genesis/build.gradle.kts
+++ grandline-genesis/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
