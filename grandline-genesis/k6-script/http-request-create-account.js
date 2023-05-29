7163b9b7881d5a9cf99726511f2c0c6f5b3f7713
지인호 [Raul] <raul@daangn.com>
2023-05-29 14:51:12 +0900

    add: http-request-create-account.js (for k6)

---
+++ grandline-genesis/k6-script/http-request-create-account.js
@@ -0,0 +1,14 @@
+import { check } from "k6"
+import http from "k6/http"
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+
+export default function () {
+    const url = "http://localhost:8080/api/v1/account"
+    const data = {
+        identifier: uuidv4(),
+        password: "test-password"
+    }
+    const response = http.post(url, JSON.stringify(data), {
+        headers: { "Content-Type": "application/json" },
+    })
+}
