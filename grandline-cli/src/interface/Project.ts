f8d5d01713334c9c8fb2dc5ab6def12082fcf21d
지인호 [Raul] <raul@daangn.com>
2023-06-02 03:52:18 +0900

    fix: Project.ts (apply ProjectTag)

--- grandline-cli/src/interface/Project.ts
+++ grandline-cli/src/interface/Project.ts
@@ -1,17 +1,18 @@
 import { UUID, randomUUID } from "crypto";
 import { Moment } from "moment";
 import moment = require("moment");
+import { ProjectTag } from "./ProjectTag";
 
 export default interface Project {
     _id: UUID,
     name: string,
     description: string,
     start_at: Moment,
-    tags: string[][],
+    tags: ProjectTag[],
 }
 
 export class SimepleProject implements Project {
-    constructor(name: string, description: string, tags: string[][]) {
+    constructor(name: string, description: string, tags: ProjectTag[]) {
         this.name = name
         this.description = description
         this.start_at = this.start_at
@@ -22,7 +23,7 @@ export class SimepleProject implements Project {
     name: string
     description: string
     start_at: Moment = moment()
-    tags: string[][]
+    tags: ProjectTag[]
 }
 
 export const OFFICIAL_PROJECT_PREFIX = "grandline-"
