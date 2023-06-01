c2500e5dd3c36c9b64e170b7c8d53c263a6958e8
지인호 [Raul] <raul@daangn.com>
2023-06-02 03:52:26 +0900

    fix: ProjectInquirerContext.ts (add ProjectTags spec)

--- grandline-cli/src/inquirer/ProjectInquirerContext.ts
+++ grandline-cli/src/inquirer/ProjectInquirerContext.ts
@@ -1,6 +1,9 @@
+import { ProjectTag } from "../interface/ProjectTag"
+
 export default interface ProjectInquirerContext {
     projectName: string,
     projectDescription: string,
+    projectTags: ProjectTag[],
 }
 
 export interface ProjectInquirerContextBuilder {
@@ -9,28 +12,43 @@ export interface ProjectInquirerContextBuilder {
 
     changeProjectDescription(projectDescription: string): ProjectInquirerContextBuilder
     getProjectDescription(): string | undefined
+
+    changeProjectTags(projectTags: ProjectTag[]): ProjectInquirerContextBuilder
+    getProjectTags(): ProjectTag[] | undefined
 }
 
 export class SimpleProjectInquirerContextBuilder implements ProjectInquirerContextBuilder{
     #projectName: string | undefined
     #projectDescription: string | undefined
+    #projectTags: ProjectTag[] | undefined
 
-    constructor(projectName?: string | undefined, projectDescription?: string | undefined) {
+    constructor(
+        projectName?: string | undefined,
+        projectDescription?: string | undefined,
+        projectTags?: ProjectTag[] | undefined  
+    ) {
         this.#projectName = projectName
         this.#projectDescription = projectDescription
+        this.#projectTags = projectTags
     }
 
     changeProjectName(projectName: string): ProjectInquirerContextBuilder {
-        return new SimpleProjectInquirerContextBuilder(projectName, this.#projectDescription)
+        return new SimpleProjectInquirerContextBuilder(projectName, this.#projectDescription, this.#projectTags)
     }
     getProjectName(): string | undefined {
         return this.#projectName
     }
     changeProjectDescription(projectDescription: string): ProjectInquirerContextBuilder {
-        return new SimpleProjectInquirerContextBuilder(this.#projectName, projectDescription)
+        return new SimpleProjectInquirerContextBuilder(this.#projectName, projectDescription, this.#projectTags)
     }
     getProjectDescription(): string | undefined {
         return this.#projectDescription
+    }
+    changeProjectTags(projectTags: ProjectTag[]): ProjectInquirerContextBuilder {
+        return new SimpleProjectInquirerContextBuilder(this.#projectName, this.#projectDescription, projectTags)
+    }
+    getProjectTags(): ProjectTag[] | undefined {
+        return this.#projectTags
     }
 }
 
@@ -38,5 +56,6 @@ export function complete(builder: ProjectInquirerContextBuilder): ProjectInquire
     return {
         projectName: builder.getProjectName()!,
         projectDescription: builder.getProjectDescription()!,
+        projectTags: builder.getProjectTags()!,
     } as ProjectInquirerContext
 }
