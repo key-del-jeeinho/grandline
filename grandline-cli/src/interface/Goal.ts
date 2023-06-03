33593a3eb74bd52cfddd8637d4d01d0efb31f581
지인호 [Raul] <raul@daangn.com>
2023-06-03 17:13:42 +0900

    fix: Goal.ts (add missing properties)

--- grandline-cli/src/interface/Goal.ts
+++ grandline-cli/src/interface/Goal.ts
@@ -1,4 +1,5 @@
 import { UUID } from "crypto";
+import { Moment } from "moment";
 const GoalMetadata = {
     repeated: {
         initialSubjectiveAchievement: 0
@@ -9,11 +10,15 @@ const GoalMetadata = {
 }
 export type GoalType = keyof(typeof GoalMetadata)
 export const GoalType = Object.keys(GoalMetadata)
+type SubjectiveAchievement = typeof GoalMetadata[GoalType]["initialSubjectiveAchievement"]
 
 export default interface Goal {
     _id: UUID,
     _type: GoalType,
-
+    name: string,
+    description: string,
+    start_at: Moment,
+    subjective_achievement: SubjectiveAchievement,
 }
 
 export function getInitialSubjectiveAchievement(type: GoalType) {
