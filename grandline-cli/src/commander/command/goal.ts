f16d54cadfae76bbd0698d9cf083f1481233329c
지인호 [Raul] <raul@daangn.com>
2023-06-03 15:03:37 +0900

    add: goal.ts

---
+++ grandline-cli/src/commander/command/goal.ts
@@ -0,0 +1,41 @@
+import { Command } from "commander";
+import { randomUUID } from "crypto";
+import moment = require("moment");
+
+const GoalMetadata = {
+    repeated: {
+        initialSubjectiveAchievement: 0
+    },
+    one_off: {
+        initialSubjectiveAchievement: null
+    }
+}
+type GoalType = keyof(typeof GoalMetadata)
+const GoalType = Object.keys(GoalMetadata)
+type GoalTypeArgument = GoalType | string
+
+export function addGoalCommand(program: Command): Command {
+    return program.command('goal')
+        .arguments('<name> <description> <type>')
+        .action(async (name, description, _type: GoalTypeArgument) => {
+            if(!GoalType.includes(_type) ) throw Error(`type must be one of ${Object.keys(GoalMetadata).join(', ')}`)
+            const type = _type as GoalType
+            const goal = {
+                _id: randomUUID(),
+                _type: type,
+                name: name,
+                description: description,
+                start_at: moment(),
+                subjective_achievement: getInitialSubjectiveAchievement(type),
+            }
+            console.log(JSON.stringify(goal, null, 2))
+        })
+}
+
+function getInitialSubjectiveAchievement(type: GoalType): number | null {
+    switch(type) {
+        case "repeated": return 0
+        case "one_off": return null
+        default: return 0
+    }
+}
