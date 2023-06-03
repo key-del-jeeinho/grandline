8a8f134be6daa1e26c0b64633f968804d9a95a26
지인호 [Raul] <raul@daangn.com>
2023-06-03 17:18:48 +0900

    remove: GrandLine.ts (can alternative to GrandLineSuperset.ts)

--- grandline-cli/src/interface/GrandLine.ts
+++
@@ -1,9 +0,0 @@
-import Contributor from "./Contributor";
-import Project from "./Project";
-
-export default interface GrandLine {
-    _grandline_version: string,
-    _grandline_active: boolean,
-    project: Project,
-    contributor: Contributor[],
-}
