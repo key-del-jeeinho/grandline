4b7b2dd847c0ac9511631594475788cfc26f6119
지인호 [Raul] <raul@daangn.com>
2023-06-03 15:05:39 +0900

    fix: apply initializeCommands.ts (and remvoe unnecessary logic)

--- grandline-cli/src/index.ts
+++ grandline-cli/src/index.ts
@@ -1,34 +1,5 @@
 #!/usr/bin/env node
 import { program } from 'commander'
-import { gray, bold, italic } from 'colorette'
-import { version } from '../package.json'
-import GrandLine from './interface/GrandLine'
-import Contributor from './interface/Contributor'
-import inquireProject from './inquirer/ProjectInquirer'
-import ProjectInquirerContext from './inquirer/ProjectInquirerContext'
-import inquireContributor from './inquirer/ContributorInquirer'
-import ContributorInquirerContext from './inquirer/ContributorInquirerContext'
+import { initializeCommands } from './commander/initializeCommands'
 
-program.command('init')
-    .argument('<projectName>')
-    .action(async (projectName) => {
-        console.log(bold('🚢 init grandline project...'))
-        console.log(italic(gray('    I smell adventure!')))
-        const project = await inquireProject({
-             projectName: projectName 
-        } as ProjectInquirerContext)
-        const mainContributor = await inquireContributor({
-        } as ContributorInquirerContext)
-        const grandline = {
-            _grandline_version: version,
-            _grandline_active: true,
-            project: project,
-            contributor: [
-                mainContributor
-            ] as Contributor[]
-        } as GrandLine
-
-        console.log(JSON.stringify(grandline, null, 2))
-    })
-
-program.parse(process.argv)
+initializeCommands(program)
