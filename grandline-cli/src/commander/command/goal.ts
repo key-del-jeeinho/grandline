1f1c17dcfbb714acf52f58a09e09b6b943ad2c1d
지인호 [Raul] <raul@daangn.com>
2023-06-03 16:06:38 +0900

    fix: goal.ts (fix wrong format for grandline.goals.json)

--- grandline-cli/src/commander/command/goal.ts
+++ grandline-cli/src/commander/command/goal.ts
@@ -1,6 +1,7 @@
 import { Argument, Command } from "commander";
 import { randomUUID } from "crypto";
 import moment = require("moment");
+import { version } from "../../../package.json";
 import { GoalType, getInitialSubjectiveAchievement } from "../../interface/Goal";
 
 export function addGoalCommand(program: Command): Command {
@@ -8,16 +9,23 @@ export function addGoalCommand(program: Command): Command {
         .argument('<name>', 'name of the goal that you create')
         .argument('<description>', 'details of the goal that you create')
         .addArgument(new Argument('<type>', 'type of goal that you create (ex, repeated)').choices(GoalType))
-        .action(async (name, description, _type: GoalType) => {
-            const type = _type as GoalType
+        .action(async (name, description, type: GoalType) => {
+            const uuid = randomUUID()
+            const startAt = moment()
+            const subjectiveAchievement = getInitialSubjectiveAchievement(type)
             const goal = {
-                _id: randomUUID(),
+                _id: uuid,
                 _type: type,
                 name: name,
                 description: description,
-                start_at: moment(),
-                subjective_achievement: getInitialSubjectiveAchievement(type),
+                start_at: startAt,
+                subjective_achievement: subjectiveAchievement,
             }
-            console.log(JSON.stringify(goal, null, 2))
+            const grandline_goals = {
+                _grandline_version: version,
+                _grandline_active: true,
+                goals: [goal]
+            }
+            console.log(JSON.stringify(grandline_goals, null, 2))
         })
 }
