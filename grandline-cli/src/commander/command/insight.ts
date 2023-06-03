4b4dddde208407a84e9d900df8bbfe9fad7315ab
지인호 [Raul] <raul@daangn.com>
2023-06-03 15:56:29 +0900

    add: insight.ts

---
+++ grandline-cli/src/commander/command/insight.ts
@@ -0,0 +1,45 @@
+import { Command } from "commander";
+import { UUID, randomUUID } from "crypto";
+import moment = require("moment");
+import { version } from "../../../package.json";
+import { Moment } from "moment";
+
+export function addInsightCommand(program: Command): Command {
+    return program.command('insight')
+        .argument('<name>', 'name of insight and created file for write detail')
+        .action(async (name: string) => {
+            const insightsPathTemplate = "./insights/{name}_{createAt}.md"
+            const uuid = randomUUID()
+            const createAt = moment()
+            const insightsPath = resolveInsightPathTemplate(insightsPathTemplate, {
+                name: name,
+                uuid: uuid,
+                createAt: createAt
+            })
+            const insight = {
+                _id: uuid,
+                name: name,
+                create_at: createAt.format("YYYY.MM.DD"),
+            }
+            const grandline_insights = {
+                _grandline_version: version,
+                _grandline_active: true,
+                scan: insightsPath,
+                insights: [insight]
+            }
+            console.log(grandline_insights)
+        })
+}
+
+function resolveInsightPathTemplate(template: string, {name, uuid, createAt}: InsightPathTemplateArguments) {
+    return template.replaceAll("{name}", name.replaceAll(" ", "_"))
+        .replaceAll("{uuid}", uuid)
+        .replaceAll("{createAt}", createAt.format("YYYYMMDD_hhmmss"))
+
+}
+
+interface InsightPathTemplateArguments {
+    name: string,
+    uuid: UUID,
+    createAt: Moment
+}
