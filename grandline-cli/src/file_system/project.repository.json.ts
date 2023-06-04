912020884a1381d725065eec53713854b0bb53fe
지인호 [Raul] <raul@daangn.com>
2023-06-04 21:41:53 +0900

    project.repository.json.ts

---
+++ grandline-cli/src/file_system/project.repository.json.ts
@@ -0,0 +1,68 @@
+import Project from "../interface/Project";
+import { exists as existsJson, read as readJson, write as writeJson } from "./_json";
+import { version } from "../../package.json";
+import { Grandline_Json, getGrandlinePathFromCwd } from "./grandline.json";
+import { START_AT_FORMAT, startAtFormat } from "./moment.json";
+
+export async function exists(path?: string): Promise<boolean> {
+    if(!path) path = getGrandlinePathFromCwd()
+    return existsJson(path)
+}
+
+export async function save(project: Project, path?: string): Promise<Project> {
+    if(!path) path = getGrandlinePathFromCwd()
+    const isExists = await existsJson(path)
+    if(isExists) return update(project, path)
+    else return create(project, path)
+}
+
+export async function update(project: Project, path?: string): Promise<Project> {
+    if(!path) path = getGrandlinePathFromCwd()
+    const grandline: Grandline_Json = await readJson(path)
+    const newGrandline: Grandline_Json = {
+        ...grandline,
+        project: {
+            _id: project._id,
+            name: project.name,
+            description: project.description,
+            start_at: project.start_at.format(START_AT_FORMAT),
+            tags: project.tags,
+        }
+    }
+    await writeJson(path, newGrandline)
+    return project
+}
+
+export async function create(project: Project, path?: string): Promise<Project> {
+    if(!path) path = getGrandlinePathFromCwd()
+    const newGrandline: Grandline_Json = {
+        _grandline_version: version,
+        _grnadline_active: true,
+        project: {
+            _id: project._id,
+            name: project.name,
+            description: project.description,
+            start_at: project.start_at.format(START_AT_FORMAT),
+            tags: project.tags,
+        },
+        contributors: [],
+    }
+    await writeJson(path, newGrandline)
+    return project
+}
+
+export async function find(path?: string): Promise<Project | null> {
+    if(!path) path = getGrandlinePathFromCwd()
+
+    const isExists = await existsJson(path)
+    if(!isExists) return null
+
+    const grnadline: Grandline_Json = await readJson(path)
+    return {
+        _id: grnadline.project._id,
+        name: grnadline.project.name,
+        description: grnadline.project.description,
+        start_at: startAtFormat(grnadline.project.start_at),
+        tags: grnadline.project.tags,
+    }
+}
