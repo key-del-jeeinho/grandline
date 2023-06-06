04628158618fe4fa1a0732e4a3ff6d40f8d70bfd
지인호 [Raul] <raul@daangn.com>
2023-06-06 20:54:09 +0900

    fix: moment.json.ts (refactor specs)

--- grandline-cli/src/file_system/moment.json.ts
+++ grandline-cli/src/file_system/moment.json.ts
@@ -1,4 +1,9 @@
 import moment = require("moment")
 
-export const startAtFormat = (startAt: string) => moment(startAt, START_AT_FORMAT)
-export const START_AT_FORMAT = 'YYYY.MM.DD'
+export function momentToJson(moment: moment.Moment) {
+    return moment.format(MOMENT_FORMAT)
+}
+export function jsonToMoment(json: string) {
+    return moment(json, MOMENT_FORMAT)
+}
+const MOMENT_FORMAT = 'YYYY.MM.DD'
