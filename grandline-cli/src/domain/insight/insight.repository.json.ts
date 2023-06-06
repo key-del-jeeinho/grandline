8e4098c270069825b3dd4b70c79b14683e282efd
지인호 [Raul] <raul@daangn.com>
2023-06-07 06:02:58 +0900

    add: insight.repository.json.ts

---
+++ grandline-cli/src/domain/insight/insight.repository.json.ts
@@ -0,0 +1,85 @@
+import { getGrandlineInsightPathFromCwd, insightToJson, jsonToInsight } from "./grandline.insight.json";
+import { exists as existsJson, read as readJson, write as writeJson } from "../../global/json/_json";
+import { version } from "../../../package.json";
+import { injectable } from "inversify";
+import { InsightRepository } from "./insight.repository";
+import Insight from "./insight.domain";
+import { Grandline_Insight_Json } from "./grandline.insight.json";
+
+@injectable()
+export class InsightRepositoryJsonImpl implements InsightRepository {
+    async exists(path?: string): Promise<boolean> {
+        if(!path) path = getGrandlineInsightPathFromCwd()
+
+        const isDataBaseExists = existsJson(path)
+        if(!isDataBaseExists) return false
+
+        const grandline_insight: Grandline_Insight_Json = await readJson(path)
+        const insights = grandline_insight.insights
+
+        const isInsightExists = insights.length > 0
+        return isInsightExists
+    }
+
+    async saveAll(insights: Insight[], path?: string): Promise<Insight[]> {
+        return Promise.all(insights.map(async (insight) => this.save(insight, path)))
+    }
+
+    async save(insight: Insight, path?: string): Promise<Insight> {
+        if(!path) path = getGrandlineInsightPathFromCwd()
+        const isDataBaseExists = await existsJson(path)
+        if(isDataBaseExists) return this.update(insight, path)
+        else return this.create(insight, path)
+    }
+
+    async create(insight: Insight, path?: string): Promise<Insight> {
+        if(!path) path = getGrandlineInsightPathFromCwd()
+        const newGrandline_insight: Grandline_Insight_Json = {
+            _grandline_version: version,
+            _grandline_active: true,
+            scan: "grandline",
+            insights: [
+                insightToJson(insight)
+            ]
+        }
+        await writeJson(path, newGrandline_insight)
+        return insight
+    }
+
+    async update(insight: Insight, path?: string): Promise<Insight> {
+        if(!path) path = getGrandlineInsightPathFromCwd()
+        const grandline_insight: Grandline_Insight_Json = await readJson(path)
+        const newGrandline_insight: Grandline_Insight_Json = {
+            ...grandline_insight,
+            insights: [
+                ...grandline_insight.insights,
+                insightToJson(insight)
+            ]
+        }
+        await writeJson(path, newGrandline_insight)
+        return insight
+    }
+
+    async findById(id: string, path?: string): Promise<Insight | null> {
+        if(!path) path = getGrandlineInsightPathFromCwd()
+
+        const isExists = await this.exists(path)
+        if(!isExists) return null
+
+        const grandline_insight: Grandline_Insight_Json = await readJson(path)
+        const insights = grandline_insight.insights
+            .find((value) => value._id === id)
+
+        return insights ? jsonToInsight(insights) : null
+    }
+
+    async findAll(path?: string): Promise<Insight[]> {
+        if(!path) path = getGrandlineInsightPathFromCwd()
+
+        const isExists = await this.exists(path)
+        if(!isExists) return []
+
+        const grandline_insight: Grandline_Insight_Json = await readJson(path)
+        return grandline_insight.insights.map(jsonToInsight)
+    }
+}
