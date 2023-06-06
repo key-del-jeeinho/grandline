262443435043db4e87f660cd6f997dda45f479da
지인호 [Raul] <raul@daangn.com>
2023-06-07 06:03:18 +0900

    fix: insight.command.ts (apply oop components)

--- grandline-cli/src/domain/insight/insight.command.ts
+++ grandline-cli/src/domain/insight/insight.command.ts
@@ -1,9 +1,8 @@
 import { Command } from "commander";
-import { UUID, randomUUID } from "crypto";
-import moment = require("moment");
-import { Moment } from "moment";
-import { GrandLineSuperset } from "../../global/GrandLineSuperset";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { SimpleInsight } from "./insight.domain";
+import { bold } from "colorette";
+import { CreateInsightCase } from "./insight.usecase.create";
 
 export interface InsightCommand {
     addInsightCommand(program: Command): Command
@@ -13,41 +12,18 @@ export const InsightCommand = Symbol.for("InsightCommand")
 
 @injectable()
 export class InsightCommandImpl implements InsightCommand {
+    constructor(
+        @inject(CreateInsightCase) private readonly createInsightCase: CreateInsightCase
+    ) {}
+
     addInsightCommand(program: Command): Command {
         return program.command('insight')
             .argument('<name>', 'name of insight and created file for write detail')
             .action(async (name: string) => {
-                const insightsPathTemplate = "./insights/{name}_{uuid}.md"
-                const uuid = randomUUID()
-                const createAt = moment()
-                const insightsPath = this.resolveInsightPathTemplate(insightsPathTemplate, {
-                    name: name,
-                    uuid: uuid,
-                    createAt: createAt
-                })
-                const insight = {
-                    _id: uuid,
-                    name: name,
-                    create_at: createAt.format("YYYY.MM.DD"),
-                }
-                const grandline_insights = GrandLineSuperset({
-                    scan: insightsPathTemplate,
-                    insights: [insight]
-                })
-                console.log(grandline_insights)
+                const insight = new SimpleInsight(name)
+                this.createInsightCase.createInsight(insight)
+
+                console.log(bold(`new insight created! id: ${insight._id}`))
             })
     }
-
-    private resolveInsightPathTemplate(template: string, {name, uuid, createAt}: InsightPathTemplateArguments) {
-        return template.replaceAll("{name}", name.replaceAll(" ", "_"))
-            .replaceAll("{uuid}", uuid)
-            .replaceAll("{createAt}", createAt.format("YYYYMMDD_hhmmss"))
-
-    }
-}
-
-interface InsightPathTemplateArguments {
-    name: string,
-    uuid: UUID,
-    createAt: Moment
 }
