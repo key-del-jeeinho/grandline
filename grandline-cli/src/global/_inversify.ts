4014d87bfb83a93163aee4eb099c60cd05b097db
지인호 [Raul] <raul@daangn.com>
2023-06-08 16:17:26 +0900

    add: _inversify.ts

--- grandline-cli/src/global/_inversify.ts
+++ grandline-cli/src/global/_inversify.ts
@@ -1,5 +1,16 @@
+import "reflect-metadata";
 import { Container } from "inversify";
+import bindGoal from "../domain/goal/goal.inversify";
+import bindInitialize from "../domain/initialize/initialize.inversify";
+import bindContributor from "../domain/contributor/contributor.inversify";
+import bindInsight from "../domain/insight/insight.inversify";
+import bindProject from "../domain/project/project.inversify";
 
 const container = new Container()
+bindContributor(container)
+bindGoal(container)
+bindInitialize(container)
+bindInsight(container)
+bindProject(container)
 
 export default container
