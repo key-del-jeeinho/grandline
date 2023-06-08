3ba20cc937dde3029e5482707120daeabc08d6b7
지인호 [Raul] <raul@daangn.com>
2023-06-08 16:26:39 +0900

    fix: _initialize.command.ts (apply inversify and oop paradime)

--- grandline-cli/src/global/_initialize.command.ts
+++ grandline-cli/src/global/_initialize.command.ts
@@ -1,20 +1,35 @@
-// import { Command } from "commander"
-// import { addInitCommand } from "../domain/initialize/initialize.command"
-// import { addGoalCommand } from "../domain/goal/goal.command"
-// import { addInsightCommand } from "../domain/insight/insight.command"
+import { Command } from "commander"
+import { InitializeCommand } from "../domain/initialize/initialize.command"
+import { GoalCommand } from "../domain/goal/goal.command"
+import { InsightCommand } from "../domain/insight/insight.command"
+import { inject, injectable } from "inversify"
 
-// type GrandlineCommand = "goal" | "init" | "insight"
+type GrandlineCommand = "goal" | "init" | "insight"
 
-// const grandlineCommands: () => {[command in GrandlineCommand] : {initialize: (program: Command) => Command}}  =
-//     () => { 
-//         return {
-//             "init": { initialize: addInitCommand },
-//             "goal": { initialize: addGoalCommand },
-//             "insight": { initialize: addInsightCommand }
-//         }
-//     }
-    
-// export function initializeCommands(program: Command) {
-//     Object.values(grandlineCommands())
-//         .map(({initialize}) => initialize(program))
-// }
+export interface GrandlineCommandInitializer {
+    initializeCommands(program: Command): void
+}
+
+export const GrandlineCommandInitializer = Symbol.for("GrandlineCommandInitializer")
+
+@injectable()
+export class CentralizedGrandlineCommandInitializer {
+    constructor(
+        @inject(InitializeCommand) private readonly initializeCommand: InitializeCommand,
+        @inject(GoalCommand) private readonly goalCommand: GoalCommand,
+        @inject(InsightCommand) private readonly insightCommand: InsightCommand
+    ) {}
+
+    getGrandlineCommands(): {[command in GrandlineCommand] : {initialize: (program: Command) => Command}} {
+        return  {
+            "init": { initialize: (command) => this.initializeCommand.addInitCommand(command) },
+            "goal": { initialize: (command) => this.goalCommand.addGoalCommand(command) },
+            "insight": { initialize: (command) => this.insightCommand.addInsightCommand(command) }
+        }
+    }
+        
+    initializeCommands(program: Command) {
+        Object.values(this.getGrandlineCommands())
+            .map(({initialize}) => initialize(program))
+    }
+}
