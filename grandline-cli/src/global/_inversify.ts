dc972ef4a0ce2e4759f1b72f818489e1c6e76895
지인호 [Raul] <raul@daangn.com>
2023-06-08 16:27:45 +0900

    fix: _inversify.ts (add binder for command initializer)

--- grandline-cli/src/global/_inversify.ts
+++ grandline-cli/src/global/_inversify.ts
@@ -5,12 +5,19 @@ import bindInitialize from "../domain/initialize/initialize.inversify";
 import bindContributor from "../domain/contributor/contributor.inversify";
 import bindInsight from "../domain/insight/insight.inversify";
 import bindProject from "../domain/project/project.inversify";
+import { CentralizedGrandlineCommandInitializer, GrandlineCommandInitializer } from "./_initialize.command";
 
 const container = new Container()
+
+bindCommandInitializer(container)
 bindContributor(container)
 bindGoal(container)
 bindInitialize(container)
 bindInsight(container)
 bindProject(container)
+
+function bindCommandInitializer(container: Container) {
+    container.bind<GrandlineCommandInitializer>(GrandlineCommandInitializer).to(CentralizedGrandlineCommandInitializer)
+}
 
 export default container
