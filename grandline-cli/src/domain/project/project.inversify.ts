5df1e7aa5247697aab263d4d310c6d05caadace7
지인호 [Raul] <raul@daangn.com>
2023-06-08 18:30:35 +0900

    fix: project.inversify.ts (add bind for InquireOverwriteProjectCase)

--- grandline-cli/src/domain/project/project.inversify.ts
+++ grandline-cli/src/domain/project/project.inversify.ts
@@ -4,9 +4,11 @@ import { ProjectRepository } from "./repository/project.repository";
 import { ProjectRepositoryJsonImpl } from "./repository/project.repository.json";
 import { CreateProjectCase } from "./usecase/project.usecase.create";
 import { InquireProjectCase } from "./usecase/project.usecase.inquire";
+import { InquireOverwriteProjectCase } from "./usecase/project.usecase.inquire_overwrite";
 
 export default function bind(container: Container) {
     container.bind<ProjectRepository>(ProjectRepository).to(ProjectRepositoryJsonImpl)
     container.bind<CreateProjectCase>(CreateProjectCase).to(ProjectService)
     container.bind<InquireProjectCase>(InquireProjectCase).to(ProjectService)
+    container.bind<InquireOverwriteProjectCase>(InquireOverwriteProjectCase).to(ProjectService)
 }
