6482ff1a9f7f1c7b2c3c3e9265122c7c66c3013c
지인호 [Raul] <raul@daangn.com>
2023-06-08 18:33:47 +0900

    fix: initialize.command (refactor and add overwrite feature)

--- grandline-cli/src/domain/initialize/initialize.command.ts
+++ grandline-cli/src/domain/initialize/initialize.command.ts
@@ -1,4 +1,4 @@
-import { gray, bold, italic } from 'colorette'
+import { gray, bold, italic, red } from 'colorette'
 import InquireProject from '../project/domain/inquire_project.domain'
 import InquireContributor from '../contributor/domain/inquire_contributor.domain'
 import { Command } from 'commander'
@@ -7,6 +7,8 @@ import { CreateContributorCase } from '../contributor/usecase/contributor.usecas
 import { inject, injectable } from 'inversify'
 import { InquireContributorCase } from '../contributor/usecase/contributor.usecase.inquire'
 import { InquireProjectCase } from '../project/usecase/project.usecase.inquire'
+import { QueryGrandlineCase } from '../grandline/grandline.usecase.query'
+import { InquireOverwriteProjectCase } from '../project/usecase/project.usecase.inquire_overwrite'
 
 export interface InitializeCommand {
     addInitCommand(program: Command): Command
@@ -17,6 +19,8 @@ export const InitializeCommand = Symbol.for("InitializeCommand")
 @injectable()
 export class InitializeCommandImpl implements InitializeCommand {
     constructor(
+        @inject(QueryGrandlineCase) private readonly queryGrandlineCase: QueryGrandlineCase,
+        @inject(InquireOverwriteProjectCase) private readonly inquireOverwriteProjectCase: InquireOverwriteProjectCase,
         @inject(CreateProjectCase) private readonly createProjectCase: CreateProjectCase,
         @inject(InquireProjectCase) private readonly inquireProjectCase: InquireProjectCase,
         @inject(CreateContributorCase) private readonly createContributorCase: CreateContributorCase,
@@ -27,17 +31,65 @@ export class InitializeCommandImpl implements InitializeCommand {
         return program.command('init')
             .argument('[projectName]')
             .action(async (projectName) => {
-                console.log(bold('🚢 init grandline project...'))
-                console.log(italic(gray('    I smell adventure!')))
-                const project = await this.inquireProjectCase.inquireProject({
-                    projectName: projectName 
-                } as InquireProject)
-                this.createProjectCase.createProject(project)
+                this.noticeExecuteToUser()
 
-                const mainContributor = await this.inquireContributorCase.inquireContributor({} as InquireContributor)
-                this.createContributorCase.createContributor(mainContributor)
+                const status = await this.checkOverwrite()
+                if(status[0] === 'cancel') {
+                    this.noticeCancelToUser(status[1])
+                    return
+                }
+                this.noticeProgressToUser(status[0])
 
-                console.log(bold("project initializing complete!"))
+                await this.initializeProject(projectName)
+                await this.initializeContributor()
+
+                this.noticeCompleteToUser(status[0])
             })
+    }
+
+    private noticeExecuteToUser() {
+        console.log(bold('🚢 init grandline project...'))
+        console.log(italic(gray('    I smell adventure!')))
+    }
+
+    private async checkOverwrite(): Promise<['cancel', string] | ['overwrite'] | ['initialize']> {
+        const isAlreadyInitialize = await this.queryGrandlineCase.exists()
+        if(isAlreadyInitialize) {
+            const doOverwrite = await this.inquireOverwriteProjectCase.inquireOverwriteProject()
+            if(!doOverwrite) return ['cancel', 'already initialized']
+            else return ['overwrite']
+        } else return ['initialize']
+    }
+
+    private async initializeProject(projectName: any) {
+        const project = await this.inquireProjectCase.inquireProject({
+            projectName: projectName 
+        } as InquireProject)
+        this.createProjectCase.createProject(project)
+    }
+    private async initializeContributor() {
+        const mainContributor = await this.inquireContributorCase.inquireContributor({} as InquireContributor)
+        this.createContributorCase.createContributor(mainContributor)
+    }
+
+    private noticeCancelToUser(reason: string) {
+        const canceled = red('canceled')
+        console.log(gray(italic(`project initializing ${canceled} cause ${reason}`)))
+    }
+
+    private noticeProgressToUser(status: 'overwrite' | 'initialize') {
+        const displayActionName = {
+            overwrite: 'overwrite',
+            initialize: 'initialize'
+        }[status]
+        console.log(gray(italic(`${displayActionName} grandline started!`)))
+    }
+    
+    private noticeCompleteToUser(status: 'overwrite' | 'initialize') {
+        const displayActionName = {
+            overwrite: 'overwriting',
+            initialize: 'initializing'
+        }[status]
+        console.log(bold(`project ${displayActionName} complete!`))
     }
 }
