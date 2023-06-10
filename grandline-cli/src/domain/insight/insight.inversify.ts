6a6b982d2e5f982d4985fd34cec33131f6f8e887
지인호 [Raul] <raul@daangn.com>
2023-06-11 08:52:37 +0900

    fix: insight.inversify.ts (apply InsightContentRepository)

--- grandline-cli/src/domain/insight/insight.inversify.ts
+++ grandline-cli/src/domain/insight/insight.inversify.ts
@@ -4,8 +4,11 @@ import { InsightService } from "./insight.service";
 import { CreateInsightCase } from "./insight.usecase.create";
 import { InsightRepository } from "./repository/insight.repository";
 import { InsightRepositoryJsonImpl } from "./repository/insight.repository.json";
+import { InsightContentRepository } from "./repository/insight_content.repository";
+import { InsightContentRepositoryJsonImp } from "./repository/insight_content.repository.json";
 
 export default function bind(container: Container) {
+    container.bind<InsightContentRepository>(InsightContentRepository).to(InsightContentRepositoryJsonImp)
     container.bind<InsightRepository>(InsightRepository).to(InsightRepositoryJsonImpl)
     container.bind<CreateInsightCase>(CreateInsightCase).to(InsightService)
     container.bind<InsightCommand>(InsightCommand).to(InsightCommandImpl)
