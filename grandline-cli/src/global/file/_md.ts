d81c26e9c4d692d201b183517a5635c0cbd0a810
지인호 [Raul] <raul@daangn.com>
2023-06-11 09:23:25 +0900

    fix: _md.ts (resolve error when directory not found)

--- grandline-cli/src/global/file/_md.ts
+++ grandline-cli/src/global/file/_md.ts
@@ -1,6 +1,9 @@
-import { pathExists, readFile, writeFile } from "fs-extra";
+import { ensureDir, pathExists, readFile, writeFile } from "fs-extra";
+import _path = require("path");
 
 export async function write(path: string, md: any): Promise<void> {
+    const directoryPath = _path.dirname(path);
+    await ensureDir(directoryPath)
     return writeFile(path, md)
 }
 
