a38dcc5b1ee318201e1394becaa5cdea41f575df
지인호 [Raul] <raul@daangn.com>
2023-06-12 14:26:07 +0900

    fix: insight_content.output.edit.ts (advencement editor by os)

--- grandline-cli/src/domain/insight/output/insight_content.output.edit.ts
+++ grandline-cli/src/domain/insight/output/insight_content.output.edit.ts
@@ -1,6 +1,8 @@
 import { injectable } from "inversify"
 import Insight from "../insight.domain"
 import { open as openNano } from "../../../global/editor/_nano"
+import { open as openNotePad } from "../../../global/editor/_notepad"
+import os = require("os")
 
 export interface EditInsightContentOutput {
     editContent(insight: Insight): Promise<Insight>
@@ -11,7 +13,9 @@ export const EditInsightContentOutput = Symbol("EditInsightContentOutput")
 @injectable()
 export class EditInsightContentOutputJsonImpl implements EditInsightContentOutput {
     async editContent(insight: Insight): Promise<Insight> {
-        openNano(insight.content_path)
+        if(os.platform() === "win32") openNotePad(insight.content_path)
+        else if (os.platform() === "darwin") openNano(insight.content_path)
+        else throw new Error("Unsupported platform")
         return insight
     }
 }
